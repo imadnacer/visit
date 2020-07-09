e9d45ca129b920db057f1cf7002879d219ee9b6a
NACER Imad Eddine <PRFL9568@ad-subs.w2k.francetelecom.fr>
2020-07-09 12:04:55 +0200

    init

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  slideOpts = {
+    speed: 400,
+    direction: 'vertical'
+  };
+
+  constructor() {}
+
+}
